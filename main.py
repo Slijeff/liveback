db4df469409a4ee2d843dac386cf1df55ddfb67a
Jeffrey Hui <31459252+Slijeff@users.noreply.github.com>
2026-01-17 16:19:23 -0600

    feat: implement YFinance data client for historical stock data retrieval

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from src.data.data_client import YFinanceDataClient
+import pandas as pd
 def main():
-    print("Hello from liveback!")
+    dc = YFinanceDataClient(["AAPL", "NVDA"], pd.to_datetime("2025-12-01"), pd.to_datetime("2026-01-01"))
+    for event in dc.stream():
+        print(event)
 
 
 if __name__ == "__main__":
